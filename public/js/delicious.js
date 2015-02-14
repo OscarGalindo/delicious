6101d530b1792c8edbe92342a2b92dd59cfcb55d
kaseOga <galindero@gmail.com>
2015-02-14 15:18:40 +0100

    Login form with modal

--- public/js/delicious.js
+++ public/js/delicious.js
@@ -3,7 +3,20 @@
  */
 'use strict';
 
-(function(){
-    $(document).ready(function() {
+(function () {
+    $(document).ready(function () {
+        $(document).on('click', '#login_link', function (e) {
+            Custombox.open({
+                target: '/delicious/public/modal/login.html',
+                effect: 'blur',
+                speed: 5,
+                overlaySpeed: 200
+            });
+            $(document).on('click', '#modalRegister', function(e) {
+                Custombox.close();
+                document.location.href = '/delicious/public/user/register';
+            });
+            e.preventDefault();
+        });
     });
 })();

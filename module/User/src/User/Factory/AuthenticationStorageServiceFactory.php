7b0f09b88b01e7ff764018179ca95050579944a0
kaseOga <kaseOga@kaseOga.local>
2015-02-24 16:49:43 +0100

    AuthenticationStorageService and Factory

---
+++ module/User/src/User/Factory/AuthenticationStorageServiceFactory.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * xenFramework (http://xenframework.com/)
+ *
+ * This file is part of the xenframework package.
+ *
+ * (c) Ismael Trascastro <itrascastro@xenframework.com>
+ *
+ * @link        http://github.com/xenframework for the canonical source repository
+ * @copyright   Copyright (c) xenFramework. (http://xenframework.com)
+ * @license     MIT License - http://en.wikipedia.org/wiki/MIT_License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace User\Service\Factory;
+
+
+use User\Service\AuthenticationStorageService;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+
+class AuthenticationStorageServiceFactory implements FactoryInterface
+{
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = $serviceLocator->get('config');
+        $appName = $config['application']['name'];
+
+        return new AuthenticationStorageService($appName);
+    }
+}

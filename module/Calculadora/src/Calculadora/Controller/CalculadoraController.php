09e95c5031634d2b85584b1878c71ff805b90d2a
kaseOga <kaseOga@kaseOga.local>
2015-03-13 18:25:32 +0100

    Creado controller + Vistas

---
+++ module/Calculadora/src/Calculadora/Controller/CalculadoraController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Calculadora\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class CalculadoraController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function SumaAction()
+    {
+        return new ViewModel();
+    }
+
+    public function RestaAction()
+    {
+        return new ViewModel();
+    }
+
+    public function MultiplicacionAction()
+    {
+        return new ViewModel();
+    }
+
+    public function DivisionAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
